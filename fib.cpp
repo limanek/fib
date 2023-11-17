a9122ebacb73377fa4fd4343d95b185fd567373c
Blazej Leman <blazejleman789@gmail.com>
2023-11-17 17:30:44 +0100

    fib_v0.0.2 add fibonacci_recursive

--- fib.cpp
+++ fib.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 
 
-int fibonacci_iterative(int number) {
+int fibonacci_iterative(int sequence) {
     int fib1 = 0;
     int fib2 = 1;
     int fib3 = 0;
-    for (size_t i = 0; i < number; i++)
+    for (size_t i = 0; i < sequence; i++)
     {
         fib3 = fib1 + fib2;
-        std::cout << fib3 << std::endl;
         fib1 = fib2;
-        std::cout << fib1 << std::endl;
         fib2 = fib3;
-        std::cout << fib2 << std::endl;
-        std::cout << std::endl;
     }
     return fib2;
 }
 
+int fibonacci_recursive(int sequence) {
+
+}
+
 int main () {
     std::cout << fibonacci_iterative(18);
+    std::cout << fibonacci_recursive(18);
 }
-//int fibonacci_recursive(int sequence);
