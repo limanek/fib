483841ffdce150da847ce7ae9b0416408e480bbf
Blazej Leman <blazejleman789@gmail.com>
2023-11-17 17:35:53 +0100

    fib_v1.0.0

--- fib.cpp
+++ fib.cpp
@@ -5,7 +5,7 @@ int fibonacci_iterative(int sequence) {
     int fib1 = 0;
     int fib2 = 1;
     int fib3 = 0;
-    for (size_t i = 0; i < sequence; i++)
+    for (size_t i = 0; i < sequence -1; i++)
     {
         fib3 = fib1 + fib2;
         fib1 = fib2;
@@ -15,10 +15,13 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
-
+    if (sequence == 1 || sequence == 2) {
+        return 1;
+    }
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
 
 int main () {
-    std::cout << fibonacci_iterative(18);
-    std::cout << fibonacci_recursive(18);
+    std::cout << fibonacci_iterative(18) << std::endl;
+    std::cout << fibonacci_recursive(18) << std::endl;
 }
